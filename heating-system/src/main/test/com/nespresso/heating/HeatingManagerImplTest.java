88c070c2d3b222f98f79453c14e8503b2d901abd
Soufiane Elharrak <s.elharrak@sii.ma>
2016-02-11 13:06:38 +0000

    first commit

---
+++ heating-system/src/main/test/com/nespresso/heating/HeatingManagerImplTest.java
@@ -0,0 +1,14 @@
+package com.nespresso.heating;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HeatingManagerImplTest {
+
+	@Test
+	public void testManageHeating() {
+		fail("Not yet implemented");
+	}
+
+}
